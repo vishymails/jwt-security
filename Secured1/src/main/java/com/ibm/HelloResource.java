b00451484e0cd6660adf1c8112f0b247d82f0fc1
vishymails <vishymails@gmail.com>
2021-03-19 13:59:32 +0530

    day 5

---
+++ Secured1/src/main/java/com/ibm/HelloResource.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+}
---
+++ Secured1/src/main/java/com/ibm/Secured1Application.java
@@ -0,0 +1,13 @@
+package com.ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secured1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secured1Application.class, args);
+	}
+
+}
